2bc99a4129769182c5019dda0920643c0ca1daec
Onur Buyukceran <onur.buyukceran@gmail.com>
2020-12-03 18:56:21 +0400

    pipeline test

--- php/index.php
+++ php/index.php
@@ -3,6 +3,8 @@
 
 	//use Acme\Pages\Page;
 
-	echo 'Here comes the constructor:<br />';
+	echo 'Here comes the constructor.<br />';
 	 
 	$page1 = new Acme\Pages\Page();
+
+	echo '<br />Push me baby one more time.<br />';
